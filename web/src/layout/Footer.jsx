fa80593a7a02733c723712a57dbb4c698f1c66af
Rodd, Nathan Phillip <nathanprodd@gmail.com>
2025-09-30 19:08:21 -0400

    adding links to footer

--- web/src/layout/Footer.jsx
+++ web/src/layout/Footer.jsx
@@ -1,4 +1,5 @@
 import GhLogo from '../assets/github-mark-white.svg?react';
+import TwitchLogo from '../assets/twitch-icon.svg?url';
 import { useScreenSize } from '../libs/useScreenSize';
 
 const SM = "28";
@@ -9,9 +10,11 @@ export const Footer = () => {
     const { isMobile } = useScreenSize();
 
     return <div id="footer" className="footer">
-        <img src="https://www.vectorlogo.zone/logos/twitch/twitch-icon.svg" className="w-6 md: w-10 object-contain" />
-        <div className="content-center text-bone">
-            <GhLogo className="github-icon fill-current" viewBox="0 0 100 100" width={isMobile ? SM : LG} height={isMobile ? SM : LG} />
-        </div>
+        <a href="https://twitch.tv/roddzillaaa" target="_blank" rel="noopener noreferrer" className="content-center text-bone">
+            <img src={TwitchLogo} className="w-6 md:w-10 object-contain cursor-pointer hover:opacity-80 transition-opacity" />
+        </a>
+        <a href="https://github.com/nrodd/rgboo" target="_blank" rel="noopener noreferrer" className="content-center text-bone">
+            <GhLogo className="github-icon fill-current cursor-pointer hover:opacity-80 transition-opacity" viewBox="0 0 100 100" width={isMobile ? SM : LG} height={isMobile ? SM : LG} />
+        </a>
     </div>
 }
