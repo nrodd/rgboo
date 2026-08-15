1d878c580f03d1d4bb0806b86977951237b4646f
NeoBaneling <josephbishop1499@yahoo.com>
2026-08-15 16:00:54 -0400

    chore: adds typescript

---
+++ web/src/global.d.ts
@@ -0,0 +1,31 @@
+declare module "*.svg?react" {
+  import * as React from "react";
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
+
+declare module "*.svg?url" {
+  const src: string;
+  export default src;
+}
+
+// Fallbacks for different import styles
+declare module "*.svg" {
+  import * as React from "react";
+  const Component: React.ComponentType<any>;
+  export default Component;
+}
+
+declare module "*?react" {
+  import * as React from "react";
+  const Component: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default Component;
+}
+
+declare global {
+  interface Window {
+    Twitch?: any;
+  }
+}
+
+export {};
---
+++ web/src/layout/MainContent.tsx
@@ -0,0 +1,24 @@
+import ColorForm from "../components/ColorForm";
+import { TwitchEmbed } from "../components/TwitchEmbed";
+import LogoIconSrc from "../assets/pumpkin.svg";
+import * as React from "react";
+const LogoIcon = LogoIconSrc as unknown as React.ComponentType<any>;
+
+export const MainContent = () => (
+  <div id="main-content" className="main-content">
+    <div className="flex sm:flex-col items-center sm:justify-center gap-4">
+      <LogoIcon viewBox="0 0 441 409" className="w-10 h-10 sm:w-24 sm:h-24" />
+      <h1 className="m-0 leading-none text-bone text-sm sm:text-md font-bold text-center translate-y-1 sm:translate-y-0">
+        RGBOO
+      </h1>
+    </div>
+    <div className="flex flex-col md:flex-row-reverse justify-center md:items-start gap-12 md:gap-8">
+      <div className="w-full md:flex-1 max-w-2xl">
+        <TwitchEmbed channel="roddzillaaa" />
+      </div>
+      <div className="w-full md:w-auto md:flex-shrink-0">
+        <ColorForm />
+      </div>
+    </div>
+  </div>
+);
