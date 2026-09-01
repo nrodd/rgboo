cd1f6bacb2f5ed2f537d6d3c28a209b4d6538294
dmboone <destaj.db@gmail.com>
2026-09-01 16:03:41 -0400

    web/44: stream fill entire screen hehe

--- web/src/layout/MainContent.tsx
+++ web/src/layout/MainContent.tsx
@@ -1,22 +1,20 @@
+import LogoIcon from "../assets/pumpkin.svg?react";
 import ColorForm from "../components/ColorForm";
 import { TwitchEmbed } from "../components/TwitchEmbed";
-import LogoIcon from "../assets/pumpkin.svg?react";
 
 export const MainContent = () => (
-  <div id="main-content" className="main-content">
-    <div className="flex sm:flex-col items-center sm:justify-center gap-4">
+  <div className="main-content">
+    <div className="flex lg:absolute py-4 left-0 right-0 justify-center items-center gap-4">
       <LogoIcon viewBox="0 0 441 409" className="w-10 h-10 sm:w-24 sm:h-24" />
-      <h1 className="m-0 leading-none text-bone text-sm sm:text-md font-bold text-center translate-y-1 sm:translate-y-0">
+      <h1 className="leading-none text-bone text-sm sm:text-md font-bold">
         RGBOO
       </h1>
     </div>
-    <div className="flex flex-col md:flex-row-reverse justify-center md:items-start gap-12 md:gap-8">
-      <div className="w-full md:flex-1 max-w-2xl">
-        <TwitchEmbed channel="roddzillaaa" />
-      </div>
-      <div className="w-full md:w-auto md:flex-shrink-0">
-        <ColorForm />
-      </div>
+    <div>
+      <TwitchEmbed channel="fireplace_on24" />
+    </div>
+    <div className="lg:absolute lg:bottom-0 lg:right-0 md:w-auto md:flex-shrink-0 w-full mt-12 mb-12 z-40">
+      <ColorForm />
     </div>
   </div>
 );
