d3a571bf9406fd32463c7d96fc3403430f1744ba
dmboone <destaj.db@gmail.com>
2026-09-01 21:36:36 -0400

    web/44: adjust z-index on logo

--- web/src/layout/MainContent.tsx
+++ web/src/layout/MainContent.tsx
@@ -4,7 +4,7 @@ import { TwitchEmbed } from "../components/TwitchEmbed";
 
 export const MainContent = () => (
   <div className="main-content">
-    <div className="flex lg:absolute py-4 left-0 right-0 justify-center items-center gap-4">
+    <div className="flex lg:absolute py-4 left-0 right-0 justify-center items-center gap-4 z-50">
       <LogoIcon viewBox="0 0 441 409" className="w-10 h-10 sm:w-24 sm:h-24" />
       <h1 className="leading-none text-bone text-sm sm:text-md font-bold">
         RGBOO
